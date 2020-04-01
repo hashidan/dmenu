446f7e3eadda4428798f3f40f0668e47fb456595
hashidan <hoangseidel02@gmail.com>
2020-04-02 00:06:03 +0200

    moved center layout out of default

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
