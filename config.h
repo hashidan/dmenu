733158170a670cb66f33a7419f3aec4ffe161281
hashidan <hoangseidel02@gmail.com>
2020-04-02 00:27:05 +0200

    changed font to jetbrains

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Code Medium:style=Medium:size=13"
+	"JetBrainsMono Medium:style=Medium:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -26,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 4;
+static const unsigned int border_width = 3;
