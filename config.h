83f28dca36b36153780adce18d2c255d5c33fdec
hashidan <hoangseidel02@gmail.com>
2020-04-15 14:51:06 +0200

    changed border width and color

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 600;                    /* minimum width when centered */
+static int min_width = 550;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMonoMedium Nerd Font Mono:style=Medium:size=12"
+	"JetBrainsMonoMedium Nerd Font Mono:style=Medium:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -26,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 3;
+static const unsigned int border_width = 2;
