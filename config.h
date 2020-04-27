0daa9280f4c15ba73143ee2929eaa8f2a9459889
Hoang Seidel <hoangseidel02@gmail.com>
2020-04-27 08:12:55 +0000

    added incremental highlighting

--- config.h
+++ config.h
@@ -7,14 +7,16 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 550;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMonoMedium Nerd Font Mono:style=Medium:size=11"
+    "JetBrainsMonoMedium Nerd Font:style=Medium:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f9f5d7", "#282828" },
-	[SchemeSel] = { "#282828", "#ffd480" },
-	[SchemeOut] = { "#282828", "#ffd480" },
+                       /*        fg         bg       */
+    [SchemeNorm]          = { "#f9f5d7", "#282828" },
+    [SchemeSel]           = { "#282828", "#ffd480" },
+    [SchemeSelHighlight]  = { "#ffd480", "#282828" },
+    [SchemeNormHighlight] = { "#ffd480", "#222222" },
+    [SchemeOut]           = { "#282828", "#ffd480" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
