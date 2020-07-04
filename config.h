21773319fe2b445ca631b29576fb44c5e5643da9
Hoang Seidel <hoangseidel02@gmail.com>
2020-07-04 12:38:22 +0200

    ignore binaries

--- config.h
+++ config.h
@@ -2,25 +2,26 @@
 /* Default settings; can be overriden by command line. */
 
 static int instant = 0;
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 550;                    /* minimum width when centered */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;    /* -c option; centers dmenu on screen */
+static int min_width = 550; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
     "JetBrainsMonoMedium Nerd Font Mono:style=Medium:size=10",
-    "FontAwesome:style=Regular:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+    "Noto Color Emoji:style=Regular:pixelsize=12:antialias=true:autohint=true",
+    "FontAwesome:style=Regular:size=10"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-                       /*        fg         bg       */
-    [SchemeNorm]          = { "#f9f5d7", "#282828" },
-    [SchemeSel]           = { "#282828", "#bbbbbb" },
-    [SchemeSelHighlight]  = { "#bbbbbb", "#282828" },
-    [SchemeNormHighlight] = { "#bbbbbb", "#222222" },
-    [SchemeOut]           = { "#282828", "#bbbbbb" },
+    /*        fg         bg       */
+    [SchemeNorm] = {"#f9f5d7", "#282828"},
+    [SchemeSel] = {"#282828", "#bbbbbb"},
+    [SchemeSelHighlight] = {"#bbbbbb", "#282828"},
+    [SchemeNormHighlight] = {"#bbbbbb", "#222222"},
+    [SchemeOut] = {"#282828", "#bbbbbb"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
